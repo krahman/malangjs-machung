4cb95d300a7cbe2803bc2fd1fcd3d50a99fbfdd6
Khalilur Rahman <khal@einsights.com>
2015-08-13 23:53:18 +0800

    Added intro to node materials

---
+++ server.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+var port = '4000';
+var host = 'localhost';
+
+http.createServer(function(req, res) {
+	var filepath = req.url === '/' ? './index.html' : '.' + req.url;
+
+	fs.exists(filepath, function(fileexist) {
+		if (fileexist) {
+			fs.readFile(filepath, function(error, content) {
+				if (error) {
+					res.writeHead('500', {'Content-Type': 'text/html'});
+					res.end('Internal server error');
+				} else {
+					res.writeHead('200', {'Content-Type': 'text/html'});
+					res.end(content, 'utf8');
+				}
+			});
+		} else {
+			res.writeHead('404', {'Content-Type': 'text/html'});
+			res.end('The page you requested is not damn found!');
+		}
+	});
+
+	// res.writeHead('200', {'Content-Type': 'text/html'});
+	// res.end('hello world');
+}).listen(port, host, function() {
+	console.log('The application is running on http://' + host + ':' + port);
+});
